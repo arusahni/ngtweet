3a5e6064a75cc0fdece9df9531adf3cc32f1485c
Aru Sahni <arusahni@gmail.com>
2015-07-12 15:46:06 -0400

    Correctly bind the twitterWidgetId.

--- src/twitterWidget.directive.js
+++ src/twitterWidget.directive.js
@@ -10,10 +10,13 @@ function TwitterWidget($log, TwitterWidgetFactory) {
         restrict: 'E',
         replace: true,
         transclude: true,
+        scope: {
+            twitterWidgetId: '='
+        },
         template: '<div class="ngtweet-wrapper" ng-transclude></div>',
         link: function(scope, element, attrs) {
             $log.debug('Linking', element, attrs);
-            if (attrs.twitterWidgetId && attrs.twitterWidgetId !== '') {
+            if (!angular.isUndefined(scope.twitterWidgetId)) {
                 TwitterWidgetFactory.create(attrs.twitterWidgetId, element[0]).then(function success(embed) {
                     $log.debug('Success!!!');
                 });
